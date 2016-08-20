aafd9ed48626db376842cfcb16acb9f1dcb77f92
maheshsp123 <maheshsp123@gmail.com>
2016-08-20 16:12:18 +0530

    Initial commit

---
+++ app/src/main/java/bvb/android/example/com/moad_a_div/MainActivity.java
@@ -0,0 +1,39 @@
+package bvb.android.example.com.moad_a_div;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    String LOG_CAT=MainActivity.class.getSimpleName();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(LOG_CAT, "onCreate()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(LOG_CAT, "onStart()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(LOG_CAT, "onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(LOG_CAT, "onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(LOG_CAT, "onDestroy()");
+    }
+}
